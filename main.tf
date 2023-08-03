1d9633d461f6786b0d75563a6756494d541a7972
kpapic <kresimir.papic@hotmail.com>
2023-08-03 17:06:34 +0200

    Adding new resource block

--- main.tf
+++ main.tf
@@ -22,3 +22,12 @@ resource "azurerm_virtual_network" "vnet" {
   location            = "Central US"
   resource_group_name = "1-2c13d6c8-playground-sandbox"
 }
+
+
+resource "azurerm_subnet" "subnet" {
+    name                 = "Robins"
+    resource_group_name = ""1-2c13d6c8-playground-sandbox""
+    virtual_network_name = azurerm_virtual_network.vnet.name
+    address_prefixes       = ["10.0.1.0/24"]
+}
+
